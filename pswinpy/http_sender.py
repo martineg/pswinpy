e1ea7a9df6f1355106ea74ff2f2fdfa639d23571
tormaroe <torbjorn.maro@gmail.com>
2011-05-04 14:30:34 +0200

    Implemented http sender. Sent the first successful test SMS

--- pswinpy/http_sender.py
+++ pswinpy/http_sender.py
@@ -1,8 +1,24 @@
 
+import httplib
+
 class HttpSender(object):
 
-  def __init__(self):
-    pass
+  def __init__(self, host='http://gw2-fro.pswin.com:81/'):
+    self.host = host
 
   def send(self, request):
-    pass
+    xml = request.xml()
+    webservice = httplib.HTTP("gw2-fro.pswin.com:81")
+    webservice.putrequest("POST", "/")
+    webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
+    webservice.putheader("Content-length", "%d" % len(xml))
+    webservice.endheaders()
+    webservice.send(xml)
+
+    statuscode, statusmessage, header = webservice.getreply()
+    print "Response: ", statuscode, statusmessage
+    print "headers: ", header
+    res = webservice.getfile().read()
+    print res
+    
+    
---
+++ send_test_message.py
@@ -0,0 +1,5 @@
+
+from pswinpy import *
+
+api = API('****','****')
+api.sendSms(4700000000, "test from python")
